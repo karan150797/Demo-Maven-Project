f5ad58e1cc4d9d1ef440d8b1d75a8aa144dd7e36
Karan <karansingh1507@gmail.com>
2021-11-29 21:20:55 +0530

    first commit

---
+++ src/main/java/in/ashokit/beans/EncoderDecoderUtils.java
@@ -0,0 +1,22 @@
+package in.ashokit.beans;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class EncoderDecoderUtils {
+	
+	public String encode(String text) {
+		Encoder encoder = Base64.getEncoder();
+		byte [] encode = encoder.encode(text.getBytes());
+		return new String(encode);
+	}
+	
+	public String decode(String encodedText) {
+		Decoder decoder = Base64.getDecoder();
+		byte [] decode = decoder.decode(encodedText);
+		return new String(decode);
+	}
+	
+}
+
